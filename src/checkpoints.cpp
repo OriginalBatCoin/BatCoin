c11be2e146e8525e0260ea3713d519404ff165ab
RealBatCoin <broken-mc@hotmail.com>
2014-01-13 13:55:14 +0200

    fork fixin'
    fixing the fork that happened from the 51% 'attack'

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -30,6 +30,12 @@ namespace Checkpoints
       (     8, uint256("0xbeb761843385c06afb3270735755bbb649ecd754c7cc4d813cd1aaa30dfe0fc2"))
         (     30, uint256("0x2ad37a5b3a89992ae485072738d7a5d4cfbe94deb5af4459ea90650d76a62b19"))
         (     41, uint256("0x5c53d4a7f1a3ad4fa7dae52648c6f994cef2f7969c6b3ee135855ee0f039d262"))
+        (         400, uint256("0x7145983fc3f1346c95af7a3b1720c0b01c926f9379dc3ecc2a6d9c0d180f8226"))
+       (         800, uint256("0x0e42f9edb41c08aff6a735f1fd33918d28ae505a6131984380729fbded363cef"))
+      (     1337, uint256("0x5ea56c7f7b89da5be25ef99cc1d0b788b8936ed6e4371c2955b0d52ce5d80123"))
+        (     1800, uint256("0x115b72122fcb0f7ff77cda6842b09b02951df836c7830c659fb9becf0fc874d5"))
+        (     3000, uint256("0x2e407195d4fc5571fe450e1c9ed9ce76407f547803a03dbc5410e129f12d7d15"))
+        (	  3437, uint256("0x6b10d95a9541f16c0126b22e1ba130e81f6eaf2d4d03825eb375f388f0bc0abe"))
        ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
